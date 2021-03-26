92dd62fa20285d7744b4ea74ef2cbd6974fc0dbf
Joseph Christopher Lavell <lavellj@student.douglascollege.ca>
2021-03-25 20:06:04 -0700

    first commit

---
+++ src/test/java/com/CSIS3275/homestay/HomestayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CSIS3275.homestay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomestayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
