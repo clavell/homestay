0e364c8e703b5174dbb05fcdcd981045df3402af
Joseph Christopher Lavell <lavellj@student.douglascollege.ca>
2021-03-29 13:11:55 -0700

    Revert "remove unimplemented clutter add password field"
    This reverts commit 821bd80e44dc26c2259b09005a7637c309a8a551.

--- src/main/java/com/CSIS3275/homestay/Controller/UserController.java
+++ src/main/java/com/CSIS3275/homestay/Controller/UserController.java
@@ -32,6 +32,16 @@ public class UserController {
         return "login";
     }
 
+    @PostMapping("/adduser")
+    public String addUser(User user, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "add-user";
+        }
+
+        userRepository.save(user);
+        return "redirect:/index";
+    }
+
     @GetMapping("/student-home")
     public String student_home(Model model , @RequestParam String email, @RequestParam String password ) {
         String emailr = email;
@@ -40,6 +50,42 @@ public class UserController {
 //        String id = model.addAttribute("userID", userRepository.findById().get());
         model.addAttribute("users", userRepository.findAll());
         return "index";
+    }
+
+    @GetMapping("/index")
+    public String showUserList(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+
+        model.addAttribute("user", user);
+        return "update-user";
+    }
+
+
+    @PostMapping("/update/{id}")
+    public String updateUser(@PathVariable("id") long id, User user,
+                             BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            user.setId(id);
+            return "update-user";
+        }
+
+        userRepository.save(user);
+        return "redirect:/index";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") long id, Model model) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        userRepository.delete(user);
+        return "redirect:/index";
     }
 
 
--- src/main/java/com/CSIS3275/homestay/Repository/UserRepository.java
+++ src/main/java/com/CSIS3275/homestay/Repository/UserRepository.java
@@ -11,4 +11,5 @@ import java.util.List;
 @Repository
 public interface UserRepository extends MongoRepository<User,Long> {
 
+    List<User> findByName(String name);
 }
