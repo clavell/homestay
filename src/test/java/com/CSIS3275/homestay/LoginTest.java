b6129c7ddb8991abf0f527753c8918a9615ea3df
Joseph Christopher Lavell <lavellj@student.douglascollege.ca>
2021-03-30 19:02:03 -0700

    BeforeAll in tests and new test for nonexistent pw

--- src/test/java/com/CSIS3275/homestay/LoginTest.java
+++ src/test/java/com/CSIS3275/homestay/LoginTest.java
@@ -4,6 +4,8 @@ import com.CSIS3275.homestay.Entity.User;
 import com.CSIS3275.homestay.Repository.UserRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -42,14 +44,22 @@ public class LoginTest {
     @Autowired
     private UserRepository userRepository;
 
-    @Test
-    void LoginWorks() throws Exception {
-        User user = new User();
+    private User user;
+
+    @BeforeEach
+    void CreateTheBasicUserAndUploadToDB(){
+        user = new User();
         user.setId(99);
         user.setPassword("asdf");
         user.setName("me");
         user.setEmail("me@me.com");
+        if(userRepository.findById(user.getId()) != null)
+            userRepository.delete(user);
+        userRepository.insert(user);
+    }
 
+    @Test
+    void LoginWorks() throws Exception {
         MvcResult result = mockMvc.perform(get("/logging_in", 42L)
 //                .contentType("application/json")
                 .param("email", "me@me.com")
@@ -65,12 +75,7 @@ public class LoginTest {
 
     @Test
     void failedLoginShowsLoginFailedMessage() throws Exception {
-
-        User user = new User();
-        user.setId(99);
-        user.setPassword("asdf");
-        user.setName("me");
-        user.setEmail("iwillNeverBeEntered@Ever.com");
+        user.setEmail("NeverEnterThisEmailIntoTheDB@Ever.com");
         MvcResult result = mockMvc.perform(get("/logging_in", 42L)
 //                .contentType("application/json")
                 .param("email", user.getEmail())
@@ -82,6 +87,53 @@ public class LoginTest {
 
         assertThat(content).contains(loginFailedMessage);
 
+        user.setEmail("me@me.com");
+        user.setPassword("wrong password");
+         result = mockMvc.perform(get("/logging_in", 42L)
+//                .contentType("application/json")
+                .param("email", user.getEmail())
+                .param("password", user.getPassword())
+                .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isOk())
+                .andReturn();
+         content = result.getResponse().getContentAsString();
+
+        assertThat(content).contains(loginFailedMessage);
+
     }
+
+    @Test
+    void ifFieldMissingLoginStillWorks() throws Exception {
+        MvcResult result = mockMvc.perform(get("/logging_in", 42L)
+//                .contentType("application/json")
+                .param("email", user.getEmail())
+                .param("password", user.getPassword())
+                .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isOk())
+                .andReturn();
+        String content = result.getResponse().getContentAsString();
+//just make sure that the page loads
+        assertThat(content).contains("Student Profile");
+
+    }
+
+    @Test
+    void nonexistentPasswordInDBDoesNotCrashProgram() throws Exception {
+        user.setPassword(null);
+        userRepository.save(user);
+        MvcResult result = mockMvc.perform(get("/logging_in", 42L)
+//                .contentType("application/json")
+                .param("email", user.getEmail())
+                .param("password", user.getPassword())
+                .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isOk())
+                .andReturn();
+        String content = result.getResponse().getContentAsString();
+//just make sure that the page loads
+        assertThat(content).contains("");
+
+    }
+
+
 }
 
