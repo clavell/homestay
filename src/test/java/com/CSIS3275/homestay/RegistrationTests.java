12f8b0744fac7675c31b29bfe6c32a9946dbfe8a
Joseph Christopher Lavell <lavellj@student.douglascollege.ca>
2021-03-31 18:00:24 -0700

    sanity check that user is rm between reg tests

--- src/test/java/com/CSIS3275/homestay/RegistrationTests.java
+++ src/test/java/com/CSIS3275/homestay/RegistrationTests.java
@@ -45,6 +45,12 @@ public class RegistrationTests {
     }
 
     @Test
+    void thereisNoRegistrationTestUserInTheDatabaseAfterDelete(){
+        User expectedNullUser = userRepository.findByEmail(user.getEmail());
+        assertNull(expectedNullUser);
+    }
+
+    @Test
     void registeringUserAddsThemToDatabase() throws Exception{
 
         mockMvc.perform(post("/register", 42L)
