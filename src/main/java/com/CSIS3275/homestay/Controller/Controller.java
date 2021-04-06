74f624f521245caf378a7becdfab036913499a29
Sarbjeet Kaur <kaursarbjeet651@gmail.com>
2021-04-06 03:01:50 -0700

    Cleaning Controller

--- src/main/java/com/CSIS3275/homestay/Controller/Controller.java
+++ src/main/java/com/CSIS3275/homestay/Controller/Controller.java
@@ -237,35 +237,5 @@ public class Controller {
         return "requests_admin";
     }
 
-//    @GetMapping("/edit/{id}")
-//    public String showUpdateForm(@PathVariable("id") String id, Model model) {
-//        User user = userRepository.findById(id)
-//                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-//
-//        model.addAttribute("user", user);
-//        return "update-user";
-//    }
-
-
-//    @PostMapping("/update/{id}")
-//    public String updateUser(@PathVariable("id") String id, User user,
-//                             BindingResult result, Model model) {
-//        if (result.hasErrors()) {
-//            user.setId(id);
-//            return "update-user";
-//        }
-//
-//        userRepository.save(user);
-//        return "redirect:/index";
-//    }
-
-//    @GetMapping("/delete/{id}")
-//    public String deleteUser(@PathVariable("id") String id, Model model) {
-//        User user = userRepository.findById(id)
-//                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-//        userRepository.delete(user);
-//        return "redirect:/index";
-//    }
-
 
 }
